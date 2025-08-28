82a2b337d210f11f46e09e35555f14793c3a284e
Nick Murphy <namurphy@cfa.harvard.edu>
2025-08-28 19:17:34 -0400

    Add initial noxfile.py with tests session

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+"""
+Configuration file for Nox.
+
+Nox is a task automation tool that can be used to run tests, build
+documentation, and perform other checks. Nox sessions are defined in
+`noxfile.py`.
+
+Running `nox` without arguments will run tests with the version of
+Python that `nox` is installed under.
+
+To invoke a nox session, enter the top-level directory of this
+repository and run
+
+    nox -s "<session>"
+
+where <session> is replaced with the name of the session. The quotes are
+necessary if the session name contains special characters.
+
+To list available sessions, run `nox -l`.
+
+Nox documentation: https://nox.thea.codes
+"""
+
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("pytest")
+    session.run("pytest", "--tb=short")
